feb9147b08ed824f0b77b25a7eacb56b4a6e3c17
IniOluwa Longe <starzoga1@gmail.com>
2023-11-25 11:20:15 +0100

    Project complete

---
+++ lib/main.dart
@@ -0,0 +1,196 @@
+import 'package:calculatorui/buttons.dart';
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var userQuestion = '';
+  var userAnswer = '';
+  final List<String> buttons = [
+    'C',
+    'DEL',
+    '%',
+    '/',
+    '9',
+    '8',
+    '7',
+    'x',
+    '6',
+    '5',
+    '4',
+    '-',
+    '3',
+    '2',
+    '1',
+    '+',
+    '0',
+    '.',
+    'ANS',
+    '=',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue[100],
+      body: Column(
+        children: [
+          const Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Name: IniOluwa Longe',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Matric No.: 210202017',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          Expanded(
+              // ignore: avoid_unnecessary_containers
+              child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(15),
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    userQuestion,
+                    style: const TextStyle(fontSize: 25, letterSpacing: 2.0),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(15),
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    userAnswer,
+                    style: const TextStyle(fontSize: 35),
+                  ),
+                ),
+              ],
+            ),
+          )),
+          Expanded(
+            flex: 2,
+            child: Container(
+              child: GridView.builder(
+                itemCount: buttons.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4),
+                itemBuilder: (BuildContext context, int index) {
+                  // Clear Button
+                  if (index == 0) {
+                    return MyButton(
+                      buttonTapped: () {
+                        setState(() {
+                          userQuestion = '';
+                          userAnswer = '';
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: Colors.green,
+                      textColor: Colors.white,
+                    );
+                    // Delete Button
+                  } else if (index == 1) {
+                    return MyButton(
+                      buttonTapped: () {
+                        setState(() {
+                          userQuestion = userQuestion.substring(
+                              0, userQuestion.length - 1);
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: Colors.red,
+                      textColor: Colors.white,
+                    );
+                  }
+                  // Equal Button
+                  else if (index == buttons.length - 1) {
+                    return MyButton(
+                      buttonTapped: () {
+                        setState(() {
+                          equalPressed();
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: Colors.deepPurpleAccent,
+                      textColor: Colors.white,
+                    );
+                  } else {
+                    return MyButton(
+                      buttonTapped: () {
+                        setState(() {
+                          userQuestion += buttons[index];
+                        });
+                      },
+                      buttonText: buttons[index],
+                      color: isOperator(buttons[index])
+                          ? Colors.lightBlue[50]
+                          : Colors.lightBlue,
+                      textColor: isOperator(buttons[index])
+                          ? Colors.lightBlue
+                          : Colors.white,
+                    );
+                  }
+                },
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  bool isOperator(String x) {
+    if (x == '%' || x == '/' || x == 'x' || x == '-' || x == '+' || x == '=') {
+      return true;
+    }
+    return false;
+  }
+
+  void equalPressed() {
+    String finalQuestion = userQuestion;
+    finalQuestion = finalQuestion.replaceAll('x', '*');
+    Parser p = Parser();
+    Expression exp = p.parse(finalQuestion);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+    userAnswer = eval.toString();
+  }
+}
+/**
+ * Note:
+ * The "%" operator is for solving Modulo and not Percentages
+ */
